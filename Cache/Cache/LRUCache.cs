a90ec5789eac75955d71107a5ca64099f46c7fb9
Dylan Griffith <dyl.griffith@gmail.com>
2013-04-11 19:53:56 +1000

    Added project and test project

---
+++ Cache/Cache/LRUCache.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dylan.Cache
+{
+    public class LRUCache<K, V>
+        where K : class
+        where V : class
+    {
+
+        public static int DefaultMaxSize
+        {
+            get
+            {
+                return mDefaultMaxSize;
+            }
+        }
+
+        /// <summary>
+        /// Constructs a new LRUCache with max size LRUCache.DefaultMax
+        /// </summary>
+        public LRUCache()
+        {
+            mMaxSize = DefaultMaxSize;
+            GenericConstructor();
+        }
+
+        /// <summary>
+        /// Constructs a new LRUCache with max size maxSize
+        /// </summary>
+        /// <param name="maxSize">The max size of the LRU Cache</param>
+        public LRUCache( int maxSize )
+        {
+            if ( maxSize < 1 )
+            {
+                throw new ArgumentException( "Argument maxSize must be at least 1!" );
+            }
+            mMaxSize = maxSize;
+            GenericConstructor();
+        }
+
+        private void GenericConstructor()
+        {
+            // Make the dictionary twice as big as it needs to be
+            // so as to be safe
+            mCache = new Dictionary<K, Node<K, V>>( 2 * mMaxSize );
+            mHead = null;
+            mTail = null;
+        }
+
+        /// <summary>
+        /// Gets the item indexed by the key if it is in the cache.
+        /// </summary>
+        /// <returns>The value paired with the key if it is in the cache
+        ///  and null if it is not in the cache</returns>
+        public V GetIfExists( K key )
+        {
+            if ( IsInCache( key ) )
+            {
+                Node<K, V> result = mCache[ key ];
+                BringToHead( result );
+                return result.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Tries to  get the item from the cache.
+        /// </summary>
+        /// <param name="key">The key for the item.</param>
+        /// <param name="value">The value of the item if it is in the cache and null otherwise.</param>
+        /// <returns>Returns true if the item is in the cache and false otherwise.</returns>
+        public bool TryGet( K key, out V value )
+        {
+            if ( IsInCache(key) )
+            {
+                value = GetIfExists( key );
+                return true;
+            }
+            else
+            {
+                value = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the item is in the cache.
+        /// </summary>
+        public bool IsInCache( K key )
+        {
+            return mCache.ContainsKey( key );
+        }
+
+        /// <summary>
+        /// Adds the key value pair to the cache
+        /// </summary>
+        public void Add( K key, V value )
+        {
+            if ( IsInCache( key ) )
+            {
+                // It's already in here
+                BringToHead( mCache[ key ] );
+            }
+            else
+            {
+                if ( mCache.Count == mMaxSize )
+                {
+                    RemoveLeastRecentlyUsed();
+                }
+                Node<K,V> nodeValue = new Node<K,V>(key,value);
+                mCache.Add( key, nodeValue );
+                AddAsHead( nodeValue );
+            }
+        }
+
+        private void AddAsHead( Node<K,V> newHead )
+        {
+            if ( mHead == null )
+            {
+                // List is currently empty so just add it
+                mHead = newHead;
+                mTail = newHead;
+                newHead.Next = null;
+                newHead.Previous = null;
+            }
+            else
+            {
+                // Add item to head
+                newHead.Previous = null;
+                newHead.Next = mHead;
+                mHead.Previous = newHead;
+                mHead = newHead;
+            }
+        }
+
+        private void BringToHead( Node<K,V> newHead )
+        {
+            if ( newHead == mHead )
+            {
+                // Already the head so do nothing
+            }
+            else if ( newHead == mTail )
+            {
+                // Tail is moved to head
+                mTail = newHead.Previous;
+                mTail.Next = null;
+                newHead.Next = mHead;
+                mHead.Previous = newHead;
+                newHead.Previous = null;
+                mHead = newHead;
+            }
+            else
+            {
+                // Item is neither head nor tail, and is moved to head
+                newHead.Previous.Next = newHead.Next;
+                newHead.Next.Previous = newHead.Previous;
+                newHead.Next = mHead;
+                mHead.Previous = newHead;
+                newHead.Previous = null;
+                mHead = newHead;
+            }
+        }
+
+        private void RemoveLeastRecentlyUsed()
+        {
+            if ( mCache.Count == 0 )
+            {
+                // Nothing to remove (this shouldn't happen)
+                throw new NotSupportedException();
+            }
+            else if ( mCache.Count == 1 )
+            {
+                // Only one item so just remove it
+                mCache.Remove( mTail.Key );
+                mHead = null;
+                mTail = null;
+            }
+            else
+            {
+                // Remove the tail
+                mCache.Remove( mTail.Key );
+                mTail = mTail.Previous;
+                mTail.Next = null;
+            }
+        }
+
+        #region Private members
+        private static int mDefaultMaxSize = 100;
+        private int mMaxSize;
+        private Dictionary<K, Node<K,V>> mCache;
+        private Node<K,V> mHead;
+        private Node<K,V> mTail;
+        #endregion
+    }
+
+    public class Node<K,V>
+    {
+        #region Public Members
+        public K Key
+        {
+            get
+            {
+                return mKey;
+            }
+        }
+
+        public V Value
+        {
+            get
+            {
+                return mValue;
+            }
+        }
+
+        public Node<K,V> Next
+        {
+            get
+            {
+                return mNext;
+            }
+            set
+            {
+                mNext = value;
+            }
+        }
+
+        public Node<K,V> Previous
+        {
+            get
+            {
+                return mPrevious;
+            }
+            set
+            {
+                mPrevious = value;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Node( K key, V value )
+        {
+            mKey = key;
+            mValue = value;
+            mNext = null;
+            mPrevious = null;
+        }
+        #endregion
+
+        #region Private members
+        private K mKey;
+        private V mValue;
+        private Node<K,V> mNext;
+        private Node<K,V> mPrevious;
+        #endregion
+    }
+}
---
+++ Cache/CacherTest/LRUCacheTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dylan.Cache;
+
+namespace CacheTest
+{
+    [TestClass]
+    public class LRUCacheTest
+    {
+        [TestMethod]
+        public void CacherTestEmpty()
+        {
+            LRUCache<string, string> cacher = new LRUCache<string, string>();
+            Assert.IsFalse( cacher.IsInCache( "hello" ) );
+            Assert.AreEqual( null, cacher.GetIfExists( "hello" ) );
+        }
+
+        [TestMethod]
+        public void CacherTestSingleElement()
+        {
+            LRUCache<string, string> cacher = new LRUCache<string, string>();
+            string key = "hello";
+            string value = "world";
+            cacher.Add( key, value );
+            Assert.IsTrue( cacher.IsInCache( key ) );
+            Assert.AreEqual( value, cacher.GetIfExists( key ) );
+        }
+
+        [TestMethod]
+        public void CacherTestMaxSize()
+        {
+            LRUCache<string, string> cacher = new LRUCache<string, string>( 1 );
+            string key = "hello";
+            string value = "world";
+            string key2 = "hello2";
+            string value2 = "world2";
+            cacher.Add( key, value );
+            cacher.Add( key2, value2 );
+            Assert.IsFalse( cacher.IsInCache( key ) );
+            Assert.IsTrue( cacher.IsInCache( key2 ) );
+            Assert.AreEqual( null, cacher.GetIfExists( key ) );
+            Assert.AreEqual( value2, cacher.GetIfExists( key2 ) );
+            string dummy;
+            Assert.AreEqual( false, cacher.TryGet( key, out dummy ) );
+        }
+
+        [TestMethod]
+        public void CacherTestMaxSize2()
+        {
+            LRUCache<string, string> cacher = new LRUCache<string, string>( 2 );
+            string key1 = "hello";
+            string value = "world";
+            string key2 = "hello2";
+            string value2 = "world2";
+            string key3 = "another key";
+            string value3 = "another value";
+
+            // Add two to cache
+            cacher.Add( key1, value );
+            cacher.Add( key2, value2 );
+
+            // Check both are in cache
+            Assert.IsTrue( cacher.IsInCache( key1 ) );
+            Assert.IsTrue( cacher.IsInCache( key2 ) );
+
+            // Get the item so it should be most recently accessed
+            string result;
+            Assert.AreEqual( true, cacher.TryGet( key1, out result ) );
+            Assert.AreEqual( value, result );
+
+            // Assert key2 is removed when adding another
+            cacher.Add( key3, value3 );
+            Assert.IsTrue( cacher.IsInCache( key1 ) );
+            Assert.IsFalse( cacher.IsInCache( key2 ) );
+            Assert.IsTrue( cacher.IsInCache( key3 ) );
+            Assert.AreEqual( value, cacher.GetIfExists( key1 ) );
+            Assert.AreEqual( null, cacher.GetIfExists( key2 ) );
+            Assert.AreEqual( value3, cacher.GetIfExists( key3 ) );
+        }
+
+        [TestMethod]
+        public void CacherTestMaxSize3()
+        {
+            int maxSize = 5;
+            LRUCache<string, string> cacher = new LRUCache<string, string>( maxSize );
+
+            for ( int i = 0; i < maxSize; ++i )
+            {
+                cacher.Add( i.ToString(), "value: " + i.ToString() );
+            }
+
+            cacher.GetIfExists( ( 3 ).ToString() );
+
+            cacher.Add( ( maxSize ).ToString(), "value: " + maxSize );
+            cacher.Add( ( maxSize + 1 ).ToString(), "value: " + ( maxSize + 1 ).ToString() );
+            cacher.Add( ( maxSize + 2 ).ToString(), "value: " + ( maxSize + 2 ).ToString() );
+            cacher.Add( ( maxSize + 3 ).ToString(), "value: " + ( maxSize + 3 ).ToString() );
+
+            // Check that 0,1,2,4 were removed but not 3
+            Assert.IsFalse( cacher.IsInCache( ( 0 ).ToString() ) );
+            Assert.IsFalse( cacher.IsInCache( ( 1 ).ToString() ) );
+            Assert.IsFalse( cacher.IsInCache( ( 2 ).ToString() ) );
+            Assert.IsTrue( cacher.IsInCache( ( 3 ).ToString() ) );
+            Assert.IsFalse( cacher.IsInCache( ( 4 ).ToString() ) );
+
+            // Check that the cache is getting the right values
+            Assert.AreEqual( null, cacher.GetIfExists( ( 0 ).ToString() ) );
+            Assert.AreEqual( null, cacher.GetIfExists( ( 1 ).ToString() ) );
+            Assert.AreEqual( null, cacher.GetIfExists( ( 2 ).ToString() ) );
+            Assert.AreEqual( "value: " + ( 3 ).ToString(), cacher.GetIfExists( ( 3 ).ToString() ) );
+            Assert.AreEqual( null, cacher.GetIfExists( ( 4 ).ToString() ) );
+            Assert.AreEqual( "value: " + ( 3 ).ToString(), cacher.GetIfExists( ( 3 ).ToString() ) );
+            Assert.AreEqual( "value: " + ( 3 ).ToString(), cacher.GetIfExists( ( 3 ).ToString() ) );
+            Assert.AreEqual( "value: " + ( 3 ).ToString(), cacher.GetIfExists( ( 3 ).ToString() ) );
+            Assert.AreEqual( "value: " + ( maxSize + 1 ).ToString(), cacher.GetIfExists( ( maxSize + 1 ).ToString() ) );
+            cacher.Add( ( maxSize + 1 ).ToString(), "value: " + ( maxSize + 1 ).ToString() );
+            Assert.AreEqual( "value: " + ( maxSize + 1 ).ToString(), cacher.GetIfExists( ( maxSize + 1 ).ToString() ) );
+
+        }
+    }
+}
